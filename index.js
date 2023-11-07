5e9e6728d20f40ee041e31cc52c6c9ea5f561500
GiovanniPlayz <130688385+GiovanniPlayz@users.noreply.github.com>
2023-11-07 12:26:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,25 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
 class CatAPI {
-  constructor() {
-    this.videos = [
-      "https://www.youtube.com/watch?v=VV6EkyyT0RM",
-      "https://www.youtube.com/watch?v=uyGpwCZlVN4",
-      "https://www.youtube.com/watch?v=pLXljYbdnUY"
-    ];
+  catVideo = ""
+
+  // Send cat video as a server response
+  sendCatVideo(res, catVideoUrl) {
+    res.status(200).send('<iframe src="' + catVideoUrl + '"></iframe>');
   }
 
-  getRandomVideo() {
-    const randomVideo = this.videos[Math.floor(Math.random() * this.videos.length)];
-    return randomVideo;
+  // Set the cat video URL
+  setCatVideoUrl(catVideoUrl) {
+    this.catVideo = catVideoUrl;
   }
 
-  addVideo(newVideo) {
-    this.videos.push(newVideo);
+  // Get the cat video URL
+  getCatVideoUrl() {
+    return this.catVideo;
+  }
+
+  // Play the cat video on the client-side
+  playCatVideo() {
+    const iframe = document.createElement('iframe');
+    iframe.src = this.catVideo;
+    document.body.appendChild(iframe);
   }
 }
-
-const catApi = new CatAPI();
-
-app.use(express.json());
-
-app.get('/cat', (req, res) => {
-  const randomVideo = catApi.getRandomVideo();
-  res.status(200).json({ video: randomVideo });
-});
-
-app.get("/", (req, res) => {
-  res.send("<h1>THIS IS NOT THE API!</h1><p>You got the wrong guy!</p>")
-});
-
-app.post('/cat', (req, res) => {
-  const newVideo = req.body.video;
-  catApi.addVideo(newVideo);
-  res.status(201).json({ message: "Cat video added successfully" });
-});
-
-app.listen(port, () => {
-  console.log(`Cat API listening at http://localhost:${port}`);
-});
